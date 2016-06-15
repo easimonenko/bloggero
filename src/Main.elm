9120a1743ce53889ecb53ab54bcfd763dd8f230a
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-06-15 16:10:49 +0300

    Main module with Elm architecture.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,38 @@
 module Main exposing (main)
 
 import Html exposing (..)
+import Html.App exposing (program)
 
-main = text "Bloggero"
+type alias Model =
+  {
+    title : String
+  }
+
+init =
+  (
+    {
+      title = "My Blog"
+    },
+    Cmd.none
+  )
+
+view model = header []
+  [
+    h1 [] [ text model.title ]
+  ]
+
+update action model =
+  (
+    model,
+    Cmd.none
+  )
+
+subscriptions model = Sub.none
+
+main = program
+  {
+    init = init,
+    view = view,
+    subscriptions = subscriptions,
+    update = update
+  }
