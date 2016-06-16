861f614dffddde39413c2983374b0e084a763c92
Evgeniy A. Simonenko <easimonenko@mail.ru>
2016-06-17 00:30:48 +0300

    Added header and reading of config.

--- src/Main.elm
+++ src/Main.elm
@@ -2,30 +2,84 @@ module Main exposing (main)
 
 import Html exposing (..)
 import Html.App exposing (program)
+import Html.Attributes exposing (class, href)
+
+import Http
+
+import Material
+import Material.Icon as Icon
+import Material.Layout as Layout
+import Material.Options as Options
+import Material.Snackbar as Snackbar
+
+import String
+import Task
 
 type alias Model =
   {
-    title : String
+    title : String,
+    config : String,
+    toast : String
   }
+
+type Msg =
+  Mdl Material.Msg |
+  ConfigFetchSucceed String |
+  ConfigFetchFail Http.Error |
+  ShowToast String |
+  ShowErrorToast String
 
 init =
   (
     {
-      title = "My Blog"
+      title = "",
+      config = "",
+      toast = ""
     },
-    Cmd.none
+    Task.perform ConfigFetchFail ConfigFetchSucceed (Http.getString "/config.json")
   )
 
-view model = header []
-  [
-    h1 [] [ text model.title ]
+headerView model = [ Layout.row []
+    [
+      Layout.title [] [ text model.title ],
+      Layout.spacer,
+      Layout.navigation []
+        [
+          a [ class "mdl-navigation__link", href "/#!/"] [ Icon.i "home", text "Home" ]
+        ]
+    ]
   ]
 
-update action model =
-  (
-    model,
-    Cmd.none
-  )
+drawerView = [ div [] [] ]
+
+mainView model =
+  [ main' []
+    [
+      text model.toast
+    ]
+  ]
+
+tabsView = ( [], [] )
+
+view model = Layout.render Mdl Material.model
+  [
+    Layout.fixedHeader
+  ]
+  {
+    header = headerView model,
+    drawer = drawerView,
+    main = mainView model,
+    tabs = tabsView
+  }
+
+update action model = case action of
+  {--ConfigFetchSucceed config -> Snackbar.add (Snackbar.toast () config) model
+  ConfigFetchFail Http.Timeout -> Snackbar.add (Snackbar.toast () "Timeout") model
+  ConfigFetchFail Http.NetworkError -> Snackbar.add (Snackbar.toast () "NetworkError") model
+  ConfigFetchFail (Http.UnexpectedPayload str) -> Snackbar.add (Snackbar.toast () str) model
+  ConfigFetchFail (Http.BadResponse code str) -> Snackbar.add (Snackbar.toast () ((toString code) ++ str)) model-}
+  ConfigFetchSucceed config -> ( { model | toast = config }, Cmd.none )
+  _ -> ( model, Cmd.none )
 
 subscriptions model = Sub.none
 
